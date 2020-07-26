4b8b4bfffd644bbf6247ac07fd52055c8651ae13
BabbarSher <59773617+nvkk007@users.noreply.github.com>
2020-07-26 13:56:24 +0530

    Update Curve-gen.sol

--- Curve-gen.sol
+++ Curve-gen.sol
@@ -1,4 +1,12 @@
 
+// This file is MIT Licensed.
+//
+// From: https://gist.githubusercontent.com/chriseth/f9be9d9391efc5beb9704255a8e2989d/raw/4d0fb90847df1d4e04d507019031888df8372239/snarktest.solidity
+// 
+// Copyright 2017 Christian Reitwiessner
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
 pragma solidity ^0.4.14;
 
 library Curve {
